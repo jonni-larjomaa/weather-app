7d2c25e0e22e013ac42bf87e3a07970283417b67
Jonni Larjomaa <jonni.larjomaa@solinor.com>
2016-08-29 23:17:24 +0300

    added tests

---
+++ test/claudator/weather_test.clj
@@ -0,0 +1,20 @@
+(ns claudator.weather-test
+  (:require [clojure.test :refer :all]
+            [claudator.weather :refer :all]))
+
+(deftest custom-params
+  (testing "can add custom params"
+    (is
+      (=
+        (:foo
+          (:query-params
+            (add-custom-params req-params {:foo "bar"})))
+              "bar"))))
+
+(def mock-json {:weather [{:id 50}] :main {:temp 12}})
+
+(deftest get-json
+  (testing "can parse json"
+    (let [data (parse-data mock-json)]
+      (is (= (:temp data) 12))
+      (is (= (:id data) 50)))))
